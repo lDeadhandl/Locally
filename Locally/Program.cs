bc65b9f7d658f12fa37440c2e3079a4f79eac71d
lDeadhandl <stefanvladetic@hotmail.com>
2022-11-16 17:47:57 -0500

    first commit

---
+++ Locally/Program.cs
@@ -0,0 +1,34 @@
+using Locally.Models;
+using Locally.Services;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<UserService>();
+
+builder.Services.Configure<LocallyDatabaseSettings>(
+                builder.Configuration.GetSection("LocallyDatabase"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Locally/Services/UserService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Locally.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace Locally.Services
+{
+    public class UserService
+    {
+        private readonly IMongoCollection<User> _usersCollection;
+
+        public UserService(
+            IOptions<LocallyDatabaseSettings> LocallyDatabaseSettings)
+        {
+            var mongoClient = new MongoClient(
+                LocallyDatabaseSettings.Value.ConnectionString);
+
+            var mongoDatabase = mongoClient.GetDatabase(
+                LocallyDatabaseSettings.Value.DatabaseName);
+
+            _usersCollection = mongoDatabase.GetCollection<User>(
+                LocallyDatabaseSettings.Value.UsersCollectionName);
+        }
+
+        public async Task<List<User>> GetAsync() =>
+            await _usersCollection.Find(_ => true).ToListAsync();
+
+        public async Task<User?> GetAsync(string id) =>
+            await _usersCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(User newUser) =>
+            await _usersCollection.InsertOneAsync(newUser);
+
+        public async Task UpdateAsync(string id, User updatedUser) =>
+            await _usersCollection.ReplaceOneAsync(x => x.Id == id, updatedUser);
+
+        public async Task RemoveAsync(string id) =>
+            await _usersCollection.DeleteOneAsync(x => x.Id == id);
+    }
+}
