0ff75d88cb965114903f564a4a20495268c6722f
lDeadhandl <stefan.vladetic7@gmail.com>
2022-11-23 12:57:47 -0500

    clean up

--- locallyclient/App.tsx
+++ locallyclient/App.tsx
@@ -1,8 +1,4 @@
 import React from "react";
-import { useAppDispatch, useAppSelector } from "./app/hooks";
-import { amountAdded, incremented } from "./features/SomeFeatureSlice";
-import reactLogo from "./assets/react.svg";
-import { useFetchDailyGamesQuery } from "./features/games/GamesApiSlice";
 
 import "./App.css";
 import Dashboard from "./app/components/Dashboard/Dashboard";
@@ -11,10 +7,9 @@ function App() {
   return (
     <div className="App">
       <div className="App-header">
-        <span className="App-header-logo">Logo.</span>
+        <span className="App-header-logo">cockinmyass</span>
         <span className="App-header-burger">+</span>
       </div>
-      <div></div>
       <Dashboard></Dashboard>
     </div>
   );
--- locallyclient/src/App.tsx
+++ locallyclient/src/App.tsx
@@ -1,9 +1,4 @@
 import React from "react";
-import { useAppDispatch, useAppSelector } from "./app/hooks";
-import { amountAdded, incremented } from "./features/SomeFeatureSlice";
-import reactLogo from "./assets/react.svg";
-import { useFetchDailyGamesQuery } from "./features/games/GamesApiSlice";
-
 import "./App.css";
 import Dashboard from "./app/components/Dashboard/Dashboard";
 
@@ -14,7 +9,6 @@ function App() {
         <span className="App-header-logo">Logo.</span>
         <span className="App-header-burger">+</span>
       </div>
-      <div></div>
       <Dashboard></Dashboard>
     </div>
   );
