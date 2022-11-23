deee8a4c441f0da621ab1682fdd42c3347601bab
lDeadhandl <stefan.vladetic7@gmail.com>
2022-11-23 12:43:28 -0500

    added cors for new project

--- Locally/Program.cs
+++ Locally/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins("http://localhost:3000");
+                          policy.WithOrigins("http://localhost:5173", "http://127.0.0.1:5173");
                       });
 });
 
